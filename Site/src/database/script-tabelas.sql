030868d9643e96b48dbeb3eb3a1d7a08fba3c1cb
reynald-costa <reynald.costa@sptech.school>
2024-05-20 15:00:47 -0300

    adcionando script das tabelas no banco de dados

--- Site/src/database/script-tabelas.sql
+++ Site/src/database/script-tabelas.sql
@@ -6,54 +6,30 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
+create database SaoPaulo;
+use SaoPaulo;
 
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14)
+create table usuario(
+id int primary key auto_increment,
+nome varchar(50),
+email varchar(50),
+idade int,
+senha varchar(45)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+
+create table quiz(
+idQuiz int primary key auto_increment,
+qtdPerguntas int
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table registro(
+idRegistro int,
+fkUsuario int,
+fkQuiz int,
+primary key(idRegistro, fkUsuario, fkQuiz),
+qtdPontos int,
+dtAtual datetime,
+foreign key (fkUsuario) references usuario(id),
+foreign key (fkQuiz) references quiz(idQuiz)
 );
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
