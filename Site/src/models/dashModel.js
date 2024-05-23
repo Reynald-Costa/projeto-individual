1ccf22fdcef946ef16072e3e80ccf1862f7ff18e
reynald-costa <reynald.costa@sptech.school>
2024-05-23 13:49:49 -0300

    implementando dashboard

---
+++ Site/src/models/dashModel.js
@@ -0,0 +1,14 @@
+ var database = require("../database/config");
+
+ function buscarUltimasMedidas() {
+
+     var instrucaoSql = `SELECT  qtdPontos  as pontos_usuarios ,  DATE_FORMAT(dtAtual,'%H:%i:%s') as horarioAtual FROM registro;     `;
+
+     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+     return database.executar(instrucaoSql);
+ }
+
+ module.exports = {
+     buscarUltimasMedidas
+ }
+
---
+++ Site/src/routes/dash.js
@@ -0,0 +1,11 @@
+ var express = require("express");
+ var router = express.Router();
+
+ var dashController = require("../controllers/dashController");
+
+ router.get("/buscarUltimasMedidas", function (req, res) {
+     dashController.buscarUltimasMedidas(req, res);
+ });
+
+
+ module.exports = router;
